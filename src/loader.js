e9300d31bfcd5fc156851ccfef0e2c88139369f0
estagent <estagent@chip.me>
2020-12-31 02:51:33 +0300

    mergeTranslations supports object arrays

--- src/loader.js
+++ src/loader.js
@@ -1,43 +1,43 @@
-import collection from './collection';
+import collection from './collection'
 
 const mergeLanguage = (lang, key, data) => {
-    if (!collection.translations) throw 'translations not set';
+    if (!collection.translations) throw 'translations not set'
 
-    if (!lang) throw 'lang must be  provided';
-    if (typeof lang !== 'string') throw 'target must be object';
+    if (!lang) throw 'lang must be  provided'
+    if (typeof lang !== 'string') throw 'target must be object'
 
-    if (!key) throw 'key must be provided';
-    if (typeof key !== 'string') throw 'key must be string';
+    if (!key) throw 'key must be provided'
+    if (typeof key !== 'string') throw 'key must be string'
 
-    if (!data) throw 'data must be  provided';
-    if (typeof data !== 'object') throw 'data must be object';
+    if (!data) throw 'data must be  provided'
+    if (typeof data !== 'object') throw 'data must be object'
 
-    const translations = collection.translations;
+    const translations = collection.translations
 
-    if (!translations.hasOwnProperty(lang)) translations[lang] = {};
+    if (!translations.hasOwnProperty(lang)) translations[lang] = {}
 
-    const target = translations[lang];
+    const target = translations[lang]
 
-    if (!target.hasOwnProperty(key)) target[key] = {};
+    if (!target.hasOwnProperty(key)) target[key] = {}
 
-    const obj = target[key];
+    const obj = target[key]
     for (const subKey of Object.keys(data)) {
-        obj[subKey] = data[subKey];
+        obj[subKey] = data[subKey]
     }
-};
+}
 
 const mergeTranslations = function (mixed, opts) {
     {
         if (typeof mixed === 'string') {
-            const {language, data} = opts;
+            const {language, data} = opts
             if (language) {
-                mergeLanguage(language, mixed, data);
+                mergeLanguage(language, mixed, data)
             } else {
                 for (const code of Object.keys(opts)) {
                     mergeTranslations(mixed, {
                         language: code,
                         data: opts[code],
-                    });
+                    })
                 }
             }
         } else if (typeof mixed == 'object') {
@@ -46,11 +46,13 @@ const mergeTranslations = function (mixed, opts) {
                     mergeTranslations(key, {
                         language: code,
                         data: mixed[key][code],
-                    });
+                    })
                 }
             }
-        }
+        } else if (Array.isArray(mixed))
+            mixed.forEach(translations => mergeTranslations(translations))
+        else throw 'unsupported merge input for languages'
     }
-};
+}
 
-export default mergeTranslations;
+export default mergeTranslations
